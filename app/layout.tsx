61c4f810afae62fa9370108de31d1b4e1ea77224
Tunahan Dogan <piyealar@gmail.com>
2023-06-15 16:42:29 +0300

    Bir devrin başlangıcı

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import { Nunito } from "next/font/google";
+
+const nunito = Nunito({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Swap Zone",
+  description: "Takas yapmak için en iyi güvenilir platform",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={nunito.className}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="text-orange-500 text-2xl">
+      Takaslarınızı gerçekleştirin!
+    </div>
+  );
+}
